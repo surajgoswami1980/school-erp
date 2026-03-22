00b265d088879895b3d6f15ccdd5318048f4c087
Suraj <surajgoswami853@gmail.com>
2026-03-22 20:59:03 +0530

    suraj

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! docker version 222';
+    return 'Hello World! docker version 222 working successfully';
   }
 }
